d4e2e7cfcdf190ab3237a70e1d69db0525ead4bf
steveone <steveone@gmail.com>
2019-02-17 19:21:09 -0800

    Adjusted App.js to meet airbnb eslint specs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import "./css/App.css";
-import MyNav from "./components/MyNav";
-import MyCard from "./components/Cards";
+import React, { Component } from 'react';
+import './css/App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import MyNav from './components/MyNav';
+import MyCard from './components/Cards';
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fas, faTwitter);
 
@@ -13,21 +13,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentCampaignName: "All Campaigns",
-      campaignId: 0
+      currentCampaignName: 'All Campaigns',
+      campaignId: 0,
     };
     this.handleCampaignChange = this.handleCampaignChange.bind(this);
   }
 
-  handleCampaignChange(currentCampaignName, campaignId) {
-    this.setState({ currentCampaignName, campaignId });
+  handleCampaignChange(currentCampaignNameNew, campaignIdNew) {
+    let { currentCampaignName, campaignId } = this.state;
+    if ((currentCampaignName !== currentCampaignNameNew) || (campaignId !== campaignIdNew)) {
+      campaignId = campaignIdNew;
+      currentCampaignName = currentCampaignNameNew;
+      this.setState({ currentCampaignName, campaignId });
+    }
   }
 
   render() {
+    const { campaignId } = this.state;
     return (
       <div className="App">
         <MyNav changeCampaign={this.handleCampaignChange} />
-        <MyCard campaignId={this.state.campaignId} />
+        <MyCard campaignId={campaignId} />
       </div>
     );
   }
