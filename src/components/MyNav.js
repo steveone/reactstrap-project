62ddc93ede9623626b23d1a2c006b735f726b614
steveone <steveone@gmail.com>
2019-02-17 19:45:45 -0800

    Mostly fixed MyNav.js for eslint airbnb

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import CampaignJson from '../json/campaigns';
-import Moment from "react-moment";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Moment from 'react-moment';
 
 import {
   Collapse,
@@ -13,8 +12,8 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Badge
-} from "reactstrap";
+  Badge,
+} from 'reactstrap';
 
 export default class MyNav extends React.Component {
   constructor(props) {
@@ -27,101 +26,109 @@ export default class MyNav extends React.Component {
     this.state = {
       isOpenNavBar: false,
       isOpenNavDropDown: false,
-      currentCampaignName: "All Campaigns",
+      currentCampaignName: 'All Campaigns',
       startDate: new Date(),
-      campaigns: null
+      campaigns: null,
     };
   }
-  toggleNavBar() {
-    this.setState({
-      isOpenNavBar: !this.state.isOpenNavBar
-    });
-  }
-  toggleNavDropDown() {
-    this.setState({
-      isOpenNavDropDown: !this.state.isOpenNavDropDown
-    });
-  }
-
 
 
   componentDidMount() {
-    fetch("http://localhost:8080/campaigns/")
+    fetch('http://localhost:8080/campaigns/')
       .then(response => response.json())
       .then(campaigns => this.setState({ campaigns }));
   }
 
-  changeCampaign(currentCampaignName, campaignId) {
-    //update campaignName and auto close open navbar (for mobile)
+  toggleNavBar() {
+    const { isOpenNavBar } = this.state;
     this.setState({
-      currentCampaignName,
-      isOpenNavBar: false
-
+      isOpenNavBar: !isOpenNavBar,
     });
-    this.props.changeCampaign(currentCampaignName, campaignId);
   }
 
+  toggleNavDropDown() {
+    const { isOpenNavDropDown } = this.state;
+    this.setState({
+      isOpenNavDropDown: !isOpenNavDropDown,
+    });
+  }
+
+  changeCampaign(currentCampaignName, campaignId) {
+    // update campaignName and auto close open navbar (for mobile)
+    const { changeCampaign } = this.props;
+    this.setState({
+      currentCampaignName,
+      isOpenNavBar: false,
+
+    });
+    changeCampaign(currentCampaignName, campaignId);
+  }
+
+
   render() {
-    const iconColor = "lightgray";
-    let campaigns = this.state.campaigns;
+    const {
+      campaigns,
+      isOpenNavBar,
+      isOpenNavDropDown,
+      currentCampaignName,
+      startDate,
+    } = this.state;
+    const iconColor = 'lightgray';
     const calendarStrings = {
-      lastDay: "[Yesterday] MMM DD",
-      sameDay: "[Today] MMM DD",
-      nextDay: "[Tomorrow] MMM DD",
-      sameElse: "MMM DD"
+      lastDay: '[Yesterday] MMM DD',
+      sameDay: '[Today] MMM DD',
+      nextDay: '[Tomorrow] MMM DD',
+      sameElse: 'MMM DD',
     };
-    //determine if All Campaigns should be in the drop down list or not
-    let allCampaigns =
-      this.state.currentCampaignName !== "All Campaigns" ? (
-        <DropdownItem
-          key={-1}
-          id={null}
-          onClick={() => {
-            this.changeCampaign("All Campaigns");
-          }}
-        >
-          {"All Camapaigns"}
-        </DropdownItem>
-      ) : null;
+    // determine if All Campaigns should be in the drop down list or not
+    const allCampaigns = currentCampaignName !== 'All Campaigns' ? (
+      <DropdownItem
+        key={-1}
+        id={null}
+        onClick={() => {
+          this.changeCampaign('All Campaigns');
+        }}
+      >
+        {'All Camapaigns'}
+      </DropdownItem>
+    ) : null;
     return (
       <div className="fixed-top">
-        <Navbar color="light" light expand="md" className="navWidth" >
+        <Navbar color="light" light expand="md" className="navWidth">
           <NavbarToggler onClick={this.toggleNavBar} />
           <Navbar className="d-md-none">
-            {this.state.currentCampaignName}
+            {currentCampaignName}
           </Navbar>
-          <Collapse isOpen={this.state.isOpenNavBar} navbar>
+          <Collapse isOpen={isOpenNavBar} navbar>
             <Nav className="mr-auto" navbar>
               <Dropdown
                 className="px-3"
                 nav
-                isOpen={this.state.isOpenNavDropDown}
+                isOpen={isOpenNavDropDown}
                 toggle={this.toggleNavDropDown}
               >
                 <DropdownToggle nav caret>
-                  {this.state.currentCampaignName}
+                  {currentCampaignName}
                 </DropdownToggle>
                 <DropdownMenu>
-                  {/*Display All Campaigns in drop down list if appropriate.*/}
+                  {/* Display All Campaigns in drop down list if appropriate. */}
                   {allCampaigns}
-                  {/*Loop through all campaigns to build the drop down list */}
-                  {/*check if campaigns is null and show loading until loaded*/}
+                  {/* Loop through all campaigns to build the drop down list */}
+                  {/* check if campaigns is null and show loading until loaded */}
                   {campaigns == null ? (
                     <DropdownItem key="loading">loading</DropdownItem>
                   ) : (
-                    campaigns.map((cur, pos, arr) => {
-                      return (
-                        <DropdownItem
-                          key={pos}
-                          id={cur["id"]}
-                          onClick={() => {
-                            this.changeCampaign(cur["campaignName"], cur["id"]);
-                          }}
-                        >
-                          {cur["campaignName"]}
-                        </DropdownItem>
-                      );
-                    })
+                    campaigns.map((cur, pos) => (
+                      <DropdownItem
+                        key={pos}
+                        id={cur.id}
+                        onClick={() => {
+                          this.changeCampaign(cur.campaignName, cur.id);
+                        }}
+                      >
+                        {cur.campaignName}
+                      </DropdownItem>
+                    ))
                   )}
                 </DropdownMenu>
               </Dropdown>
@@ -161,7 +168,7 @@ export default class MyNav extends React.Component {
                   fixedWidth
                 />
                 <Moment calendar={calendarStrings} className="navCalendarColor">
-                  {this.state.startDate}
+                  {startDate}
                 </Moment>
               </NavItem>
               <NavItem className="navPadding">
